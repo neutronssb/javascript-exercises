c7d048e51595a375b96f79fcb4a5a997e3f40dae
ssb <61000448+neutronssb@users.noreply.github.com>
2023-01-12 15:53:47 -0500

    added code for all exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, repeatTimes) {
+    output = ''
+    if(repeatTimes < 0){
+        return 'ERROR'
+    }
+    for (let i = repeatTimes; i > 0; i--){
+        output += inputString
+    }
+    return output
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let invalidInput = start < 0 || end < 0 || typeof start != "number" || typeof end != "number";
+    if(invalidInput){
+        return "ERROR";
+    }
+    let total = 0;
+    for(let i = Math.min(start, end); i <= Math.max(start, end); i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
